69cf821747f0e3657e5d744b34316d7ae8136f56
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2020-11-27 11:49:50 -0300

    Move gcloud commands to the startup file

--- startup/exp-01.sh
+++ startup/exp-01.sh
@@ -3,3 +3,5 @@ git clone https://github.com/igutierrezm/BLOPimpute-experiments.git
 cd BLOPimpute-experiments
 source install.sh
 julia src/exp-01.jl
+gsutil cp data/exp-01.csv gs://rivera2021-db/exp-01.csv
+gcloud compute instances stop rivera2021-vm --zone us-central1-a
