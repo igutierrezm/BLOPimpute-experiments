ffbd92c5958e1d316e7f463211f886af8cb2c229
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2020-12-02 23:17:19 -0300

    Create folder for GCP specific scripts

---
+++ gcp/main.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+gcloud beta compute \
+    --project=gcp-test-296413 instances create exp-01 \
+    --zone=us-central1-a \
+    --machine-type=e2-standard-8 \
+    --subnet=default \
+    --network-tier=PREMIUM \
+    --maintenance-policy=MIGRATE \
+    --service-account=906931621342-compute@developer.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/cloud-platform \
+    --image=ubuntu-2010-groovy-v20201111 \
+    --image-project=ubuntu-os-cloud \
+    --boot-disk-size=200GB \
+    --boot-disk-type=pd-standard \
+    --boot-disk-device-name=exp-01 \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --reservation-affinity=any \
+    --metadata-from-file startup-script=gcp/startup.sh
+
+###
+### The following code creates a RStudio server in GCE
+###
+
+# gcloud beta compute 
+#     --project=gcp-test-296413 instances create rstudio 
+#     --zone=europe-west6-a 
+#     --machine-type=e2-standard-8 
+#     --subnet=default 
+#     --network-tier=PREMIUM 
+#     --maintenance-policy=MIGRATE 
+#     --service-account=906931621342-compute@developer.gserviceaccount.com 
+#     --scopes=https://www.googleapis.com/auth/cloud-platform 
+#     --tags=rstudio,http-server 
+#     --image=ubuntu-2004-focal-v20201111 
+#     --image-project=ubuntu-os-cloud 
+#     --boot-disk-size=50GB 
+#     --boot-disk-type=pd-standard 
+#     --boot-disk-device-name=rstudio 
+#     --no-shielded-secure-boot 
+#     --shielded-vtpm 
+#     --shielded-integrity-monitoring 
+#     --reservation-affinity=any
+
+
+
+# sudo apt update -y
+# sudo apt upgrade -y
+
+# # Add CRAN repo to the lists of sources
+# sudo apt-key adv \
+#     --keyserver keyserver.ubuntu.com \
+#     --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+# sudo add-apt-repository \
+#     'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+
+# # Install R
+# sudo apt install r-base r-base-core r-recommended r-base-dev -y
+
+# # Install RStudio
+# sudo apt-get install gdebi-core
+# wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1093-amd64.deb
+# sudo gdebi rstudio-server-1.3.1093-amd64.deb --n
+
+# # Install supporting packages
+# sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev
+
+# # Add user (master) and password (123)
+# useradd -p $(openssl passwd -crypt 123) master
---
+++ gcp/startup.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+# A startup file for running any file in data/ using GOOGLE COMPUTE ENGINE
+
+# User defined parameters
+ID="01"                                        # experiment id
+VM_NAME="exp-${ID}"                            # VM instance name
+CPLEX_GCS_BIN="gs://cplex-1210/cplex-1210.bin" # cplex .bin location
+OUTPUT_GCS_CSV="gs://blopimpute/exp-${ID}.csv" # .csv destination
+# Notes:
+# - The VM must be created in us-central1-a
+# - The CPLEX_GCS_BIN and OUTPUT_GCS_CSV files must be in GCS buckets.
+
+# Install JRE 2:1.11-72
+sudo apt update
+sudo apt --assume-yes install default-jre=2:1.11-72
+
+# Install CPLEX 12.10
+gsutil cp ${CPLEX_GCS_BIN} cplex.bin
+chmod +x cplex.bin
+CPLEX_DIR="/opt"
+sudo ./cplex.bin \
+    -i silent \
+    -DINSTALLER_UI=silent \
+    -DLICENSE_ACCEPTED=TRUE \
+    -DUSER_INSTALL_DIR=${CPLEX_DIR}
+export CPLEX_STUDIO_BINARIES="${CPLEX_DIR}/cplex/bin/x86-64_linux/"
+rm -rf cplex.bin
+
+# Install Julia 1.5.3
+url="https://julialang-s3.julialang.org/bin/linux/x64"
+url="${url}/1.5/julia-1.5.3-linux-x86_64.tar.gz"
+wget ${url// /}
+tar -xvzf julia-1.5.3-linux-x86_64.tar.gz
+sudo cp -r julia-1.5.3 /opt/
+sudo ln -s /opt/julia-1.5.3/bin/julia /usr/local/bin/julia
+sudo apt --assume-yes autoremove -f
+rm -rf julia-1.5.3*
+
+# Run the experiment
+git clone https://github.com/igutierrezm/BLOPimpute-experiments.git
+cd BLOPimpute-experiments
+julia data/exp-${ID}.jl
+
+# Save results in a bucket
+gsutil cp data/exp-${ID}.csv ${OUTPUT_GCS_CSV}
+
+# Delete the vm
+gcloud compute instances delete ${VM_NAME} --zone us-central1-a --quiet
