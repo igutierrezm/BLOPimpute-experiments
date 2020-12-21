f140089ae4948e2b5d511431c8b95bef63751ece
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2020-12-21 20:14:40 -0300

    Create a dummy file for testing puposes

--- gcp/startup.sh
+++ gcp/startup.sh
@@ -29,7 +29,8 @@ sudo apt --assume-yes autoremove -f
 rm -rf julia-1.5.3*
 
 # Run experiment
-git clone https://github.com/igutierrezm/BLOPimpute-experiments.git
+echo "a" >> a.txt
+# git clone https://github.com/igutierrezm/BLOPimpute-experiments.git
 # cd BLOPimpute-experiments
 # julia -e 'using Pkg; Pkg.activate("."); Pkg.instantiate(); Pkg.build("CPLEX")'
 # julia data/exp-${ID}.jl
