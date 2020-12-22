89ca57ecefa691588a6aa85c9dc56346056b4fd3
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2020-12-22 11:04:09 -0300

    Change the covariates distribution and some parameters

--- data/exp-01.jl
+++ data/exp-01.jl
@@ -6,13 +6,13 @@ nworkers() == 8 || addprocs(8, exeflags = "--project")
 
 # Simulate a sample of size N from the DGP determined by (d, l, σ)
 function simulate_sample(N, d, l, σ)
-    K = [2, 4, 6, 10, 2]
-    X = Random.rand(K[d], N)
+    K = [2, 6, 10, 2]
+    X = Random.randn(K[d], N)
     y = σ * Random.randn(N)
     for j ∈ 1:K[d]
         @. y += X[j, :] * (-1)^(j - 1)
     end
-    d == 5 && @. y += X[1, :]^2 - X[2, :]^2 - X[1, :] * X[2, :]
+    d == 4 && @. y += X[1, :]^2 - X[2, :]^2 - X[1, :] * X[2, :]
     P = @. 0.7 / (1 + exp(1 + 1.1X[1, :] - 0.8X[2, :])) + l / 10
     M = Random.rand(N) .>= P
     X = [X[:, M .== h] for h = 0:1] |> x -> OffsetArrays.OffsetVector(x, 0:1)
@@ -32,11 +32,11 @@ function generate_model(sample)
 end
 
 # Compute all combinations of the form θ = (N, d, l, σ, r), r => sample id
-Ns = [5, 10, 20] * 100;
-ls = [0.5, 1, 2, 3];
+Ns = [2, 5, 10] * 100;
+ls = [3, 7] / 4;
 σs = [1, √2];
 rs = 1:400;
-ds = 1:5;
+ds = 1:4;
 θs = collect(Iterators.product(Ns, ds, ls, σs, rs))[:];
 
 # Set a seed
@@ -102,3 +102,8 @@ df = map([:knn, :blop]) do m
     )
 end;
 CSV.write("data/exp-01.csv", vcat(df...))
+
+# for l ∈ ls
+#     y, x = simulate_sample(1000000, 1, l, 1)
+#     println(length(y[0]) / sum(length.(y)))
+# end
