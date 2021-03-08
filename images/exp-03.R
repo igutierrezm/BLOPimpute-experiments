b48cc05dac2c264fb5eadb518caa89066957b7f0
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-03-08 22:57:10 +0000

    Update the plots incrementing the number of replications.

--- images/exp-03.R
+++ images/exp-03.R
@@ -3,12 +3,14 @@ f <- function (x, y) mean((x - y)^2)
 exp_03a <- 
     readr::read_csv("data/exp-02.csv") %>%
     dplyr::filter(l == 0.75, o == 1, !is.infinite(estimate)) %>%
+    dplyr::mutate(m = "maximum") %>%
     dplyr::group_by(m, d, N) %>%
     dplyr::summarise(mse = f(estimate, target), .groups = "drop")
 
 exp_03b <- 
     readr::read_csv("data/exp-03.csv") %>%
     dplyr::filter(l == 0.75, o == 1, !is.infinite(estimate)) %>%
+    dplyr::mutate(m = "fitted") %>%
     dplyr::group_by(m, d, N) %>%
     dplyr::summarise(mse = f(estimate, target), .groups = "drop")
 
@@ -27,6 +29,9 @@ p <-
         panel.grid.major = ggplot2::element_blank(), 
         panel.grid.minor = ggplot2::element_blank(),
         legend.position  = "top"
+    ) +
+    ggplot2::labs(
+        fill = "S"
     )
 ggplot2::ggsave("images/exp-03.pdf", p, width = 6, height = 3)
 p
